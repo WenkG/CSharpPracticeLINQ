7102118f9e26ab9272ea5271af59877f600e74ff
Wenk <Khokhkir@gmail.com>
2026-08-11 13:51:25 +0500

    Изменение вывода имени

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ class Player
     }
     public void ShowName()
     {
-        Console.WriteLine($"Имя: {Name}");
+        Console.WriteLine($"Игрок: {Name}");
     }
     public void ShowInfo()
     {
